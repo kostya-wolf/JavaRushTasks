70921afba64f399392f74c87b9a3b946a763e78e
kostya-wolf <kostya-wolf@yandex.ru>
2020-02-21 21:16:02 +0300

    task35.task3511 задача Wildcards для коллекций

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3511/Solution.java
@@ -0,0 +1,57 @@
+package com.javarush.task.task35.task3511;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/* 
+Wildcards для коллекций
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        System.out.println(sum(numbers));
+        System.out.println(multiply(numbers));
+        System.out.println(concat(numbers));
+        System.out.println(combine(Collections.singletonList(numbers)));
+    }
+
+    public static Double sum(List<? extends Number> list) {
+        Double result = 0.0;
+        for (int i = 0; i < list.size(); i++) {
+            Number numb = (Number) list.get(i);
+            result += numb.doubleValue();
+        }
+        return result;
+    }
+
+    public static Double multiply(List<? extends Number> list) {
+        Double result = 1.0;
+        for (int i = 0; i < list.size(); i++) {
+            Number numb = (Number) list.get(i);
+            result *= numb.doubleValue();
+        }
+        return result;
+    }
+
+    public static String concat(List<?> list) {
+        StringBuilder builder = new StringBuilder();
+        for (Object obj : list) {
+            builder.append(obj);
+        }
+        return builder.toString();
+    }
+
+    public static List combine(List<? extends Collection> list) {
+        List result = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            Collection collection = (Collection) list.get(i);
+            result.addAll(collection);
+        }
+        return result;
+    }
+}
