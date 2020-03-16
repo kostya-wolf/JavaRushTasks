0e0d5752646fd66b3b8ad2ce077c73b7ee34b9ec
kostya-wolf <kostya-wolf@yandex.ru>
2020-03-16 22:11:52 +0300

    task36.task3601 MVC - простая версия

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3601/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task36.task3601;
+
+/*
+MVC - простая версия
+*/
+public class Solution {
+    public static void main(String[] args) {
+        new View().fireShowDataEvent();
+    }
+
+
+
+
+
+
+}
