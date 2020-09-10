7675303ec6ec60d849e234f070744606e00cc911
kostya-wolf <kostya-wolf@yandex.ru>
2020-09-10 23:25:45 +0300

    task33.task3310 Shortener (4) - SecureRandom и BigInteger

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Helper.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task33.task3310;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class Helper {
+
+    public static String generateRandomString() {
+        return new BigInteger(130, new SecureRandom()).toString(36);
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
