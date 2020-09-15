18288e56a811b8319ba35ed4030da11781d3961f
kostya-wolf <kostya-wolf@yandex.ru>
2020-09-15 22:12:03 +0300

    task33.task3310 Shortener (7)

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/Entry.java
@@ -0,0 +1,49 @@
+package com.javarush.task.task33.task3310.strategy;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Entry implements Serializable {
+    Long key;
+    String value;
+    Entry next;
+    int hash;
+
+    public Entry(int hash, Long key, String value, Entry next) {
+        this.hash = hash;
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+
+    public Long getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(key) ^ Objects.hashCode(value);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj instanceof Entry) {
+            Entry e = (Entry) obj;
+            if (Objects.equals(key, e.getKey()) &&
+                    Objects.equals(value, e.getValue()))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return key + "=" + value;
+    }
+}
