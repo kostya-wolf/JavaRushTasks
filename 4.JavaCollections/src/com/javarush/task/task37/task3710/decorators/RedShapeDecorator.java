938ef28c5222679e1fd1d0519b59b7d5ffe335f6
kostya-wolf <kostya-wolf@yandex.ru>
2021-01-28 18:35:04 +0300

    task37.task3710 Decorator

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3710/decorators/RedShapeDecorator.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task37.task3710.decorators;
+
+import com.javarush.task.task37.task3710.shapes.Shape;
+
+public class RedShapeDecorator extends ShapeDecorator {
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    private void setBorderColor(Shape shape) {
+        System.out.println(String.format("Setting the border color for %s to red.", shape.getClass().getSimpleName()));
+    }
+
+    @Override
+    public void draw() {
+        setBorderColor(this.decoratedShape);
+        super.draw();
+    }
+}
