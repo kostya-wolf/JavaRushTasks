989f43d48d11ca832d4183edc2e8b64b4c5a8fe6
kostya-wolf <kostya-wolf@yandex.ru>
2021-02-19 12:53:14 +0300

    task39.task3913 Парсер логов (2)

--- 4.JavaCollections/src/com/javarush/task/task39/task3913/LogParser.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/LogParser.java
@@ -1,22 +1,29 @@
 package com.javarush.task.task39.task3913;
 
 import com.javarush.task.task39.task3913.query.IPQuery;
+import com.javarush.task.task39.task3913.query.UserQuery;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
-public class LogParser implements IPQuery {
+public class LogParser implements IPQuery, UserQuery {
     private Path logDir;
     private List<String> logs;
     private List<File> fileList;
 
     public LogParser(Path logDir) {
         this.logDir = logDir;
+        logsInit();
     }
 
     private void logsInit() {
@@ -96,9 +103,6 @@ public class LogParser implements IPQuery {
     }
 
     private List<String[]> getLogsAfterBefore(Date after, Date before) {
-        if (logs == null) {
-            logsInit();
-        }
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("d.M.y H:m:s");
         LocalDateTime afterLCT = after == null ? LocalDateTime.MIN : after.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
         LocalDateTime beforeLCT = before == null ? LocalDateTime.MAX : before.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
@@ -109,5 +113,91 @@ public class LogParser implements IPQuery {
                         || LocalDateTime.parse(s.split("\t")[2], dtf).isEqual(beforeLCT))
                 .map(s -> s.split("\t"))
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public Set<String> getAllUsers() {
+        return logs.stream()
+                .map(s -> s.split("\t")[1])
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public int getNumberOfUsers(Date after, Date before) {
+        return getLogsAfterBefore(after, before)
+                .stream()
+                .map(s -> s[1])
+                .collect(Collectors.toSet())
+                .size();
+    }
+
+    @Override
+    public int getNumberOfUserEvents(String user, Date after, Date before) {
+        return getLogsAfterBefore(after, before)
+                .stream()
+                .filter(s -> s[1].equals(user))
+                .map(s -> s[3].split(" ")[0])
+                .collect(Collectors.toSet())
+                .size();
+    }
+
+    @Override
+    public Set<String> getUsersForIP(String ip, Date after, Date before) {
+        return getLogsAfterBefore(after, before)
+                .stream()
+                .filter(s -> s[0].equals(ip))
+                .map(s -> s[1])
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<String> getLoggedUsers(Date after, Date before) {
+        return getDoneEventUsers(after, before, Event.LOGIN.name());
+    }
+
+    @Override
+    public Set<String> getDownloadedPluginUsers(Date after, Date before) {
+        return getDoneEventUsers(after, before, Event.DOWNLOAD_PLUGIN.name());
+    }
+
+    @Override
+    public Set<String> getWroteMessageUsers(Date after, Date before) {
+        return getDoneEventUsers(after, before, Event.WRITE_MESSAGE.name());
+    }
+
+    @Override
+    public Set<String> getSolvedTaskUsers(Date after, Date before) {
+        return getAnyTaskEventUsers(after, before, Event.SOLVE_TASK.name());
+    }
+
+    @Override
+    public Set<String> getSolvedTaskUsers(Date after, Date before, int task) {
+        return getDoneEventUsers(after, before, Event.SOLVE_TASK.name() + " " + task);
+    }
+
+    @Override
+    public Set<String> getDoneTaskUsers(Date after, Date before) {
+        return getAnyTaskEventUsers(after, before, Event.DONE_TASK.name());
+    }
+
+    @Override
+    public Set<String> getDoneTaskUsers(Date after, Date before, int task) {
+        return getDoneEventUsers(after, before, Event.DONE_TASK.name() + " " + task);
+    }
+
+    private Set<String> getDoneEventUsers(Date after, Date before, String eventName) {
+        return getLogsAfterBefore(after, before)
+                .stream()
+                .filter(s -> s[3].equals(eventName))
+                .map(s -> s[1])
+                .collect(Collectors.toSet());
+    }
+
+    private Set<String> getAnyTaskEventUsers(Date after, Date before, String eventName) {
+        return getLogsAfterBefore(after, before)
+                .stream()
+                .filter(s -> s[3].startsWith(eventName))
+                .map(s -> s[1])
+                .collect(Collectors.toSet());
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
@@ -10,5 +10,16 @@ public class Solution {
         System.out.println(logParser.getIPsForUser("Amigo", null, null));
         System.out.println(logParser.getIPsForEvent(Event.SOLVE_TASK, null, null));
         System.out.println(logParser.getIPsForStatus(Status.OK, null, null));
+        System.out.println(logParser.getAllUsers());
+        System.out.println(logParser.getNumberOfUsers(null, null));
+        System.out.println(logParser.getNumberOfUserEvents("Amigo", null, null));
+        System.out.println(logParser.getUsersForIP("127.0.0.122", null, null));
+        System.out.println(logParser.getLoggedUsers(null, null));
+        System.out.println(logParser.getDownloadedPluginUsers(null, null));
+        System.out.println(logParser.getWroteMessageUsers(null, null));
+        System.out.println(logParser.getSolvedTaskUsers(null, null));
+        System.out.println(logParser.getSolvedTaskUsers(null, null, 18));
+        System.out.println(logParser.getDoneTaskUsers(null, null));
+        System.out.println(logParser.getDoneTaskUsers(null, null, 48));
     }
 }
