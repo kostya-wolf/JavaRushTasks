e1fd130f5cdbb6b32b3c3e4ddd998133f3c66122
kostya-wolf <kostya-wolf@yandex.ru>
2021-03-04 19:27:51 +0300

    task38.task3810 Реализуй аннотации

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3810/Date.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task38.task3810;
+
+public @interface Date {
+    int year();
+
+    int month();
+
+    int day();
+
+    int hour();
+
+    int minute();
+
+    int second();
+    //напиши свой код
+}
