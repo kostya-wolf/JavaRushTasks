9294e9f84e0f1c14b4dd59b34244872f081f4c4e
kostya-wolf <kostya-wolf@yandex.ru>
2021-03-11 18:43:34 +0300

    task28.task2810 Aggregator (6)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,15 +1,25 @@
 package com.javarush.task.task28.task2810.model;
 
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
 public class HHStrategy implements Strategy {
-    private static final String URL_FORMAT = "https://grc.ua/search/vacancy?text=java+%s&page=%d";
+    private static final String URL_FORMAT = "https://hh.ru/search/vacancy?text=java+%s&page=%d";
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
-        return Collections.emptyList();
+        try {
+            Document document = Jsoup.connect(URL_FORMAT).get();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task28/task2810/vo/Vacancy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/vo/Vacancy.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task28.task2810.vo;
 
+import java.util.Objects;
+
 public class Vacancy {
     private String title;
     private String salary;
@@ -54,5 +56,23 @@ public class Vacancy {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vacancy vacancy = (Vacancy) o;
+        return Objects.equals(title, vacancy.title) &&
+                Objects.equals(salary, vacancy.salary) &&
+                Objects.equals(city, vacancy.city) &&
+                Objects.equals(companyName, vacancy.companyName) &&
+                Objects.equals(siteName, vacancy.siteName) &&
+                Objects.equals(url, vacancy.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, salary, city, companyName, siteName, url);
     }
 }
