396513b0e82c8fd83a850eb1cf933b254a9fe14b
kostya-wolf <kostya-wolf@yandex.ru>
2021-03-17 17:03:12 +0300

    task28.task2810 Aggregator (11)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -1,34 +1,18 @@
 package com.javarush.task.task28.task2810;
 
-import com.javarush.task.task28.task2810.model.Provider;
-import com.javarush.task.task28.task2810.vo.Vacancy;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.javarush.task.task28.task2810.model.Model;
 
 public class Controller {
-    private Provider[] providers;
+    private Model model;
 
-    public Controller(Provider... providers) {
-        if (providers == null || providers.length == 0) {
+    public Controller(Model model) {
+        if (model == null) {
             throw new IllegalArgumentException();
         }
-        this.providers = providers;
+        this.model = model;
     }
 
-    @Override
-    public String toString() {
-        return "Controller{" +
-                "providers=" + Arrays.toString(providers) +
-                '}';
-    }
-
-    public void scan() {
-        List<Vacancy> result = new ArrayList<>();
-        for (Provider provider : providers) {
-            result.addAll(provider.getJavaVacancies("Kiev"));
-        }
-        System.out.println(result.size());
+    public void onCitySelect(String cityName) {
+        model.selectCity(cityName);
     }
 }
