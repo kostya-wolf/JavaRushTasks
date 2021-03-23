a7bbb2ab09fd4873369a2dc6f82621a765418525
kostya-wolf <kostya-wolf@yandex.ru>
2021-03-23 17:24:01 +0300

    task28.task2810 Aggregator (15) Jsoup

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -2,9 +2,11 @@ package com.javarush.task.task28.task2810.view;
 
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
@@ -33,7 +35,39 @@ public class HtmlView implements View {
     }
 
     private String getUpdatedFileContent(List<Vacancy> vacancies) {
-        return null;
+        try {
+            Document document = getDocument();
+
+            Element template = document
+                    .getElementsByClass("template")
+                    .clone()
+                    .removeAttr("style")
+                    .removeClass("template")
+                    .first();
+
+            document
+                    .select("tr.vacancy")
+                    .not("tr.template")
+                    .remove();
+
+            for (Vacancy vacancy : vacancies) {
+                Element vacToTag = template.clone();
+                vacToTag.getElementsByClass("city").append(vacancy.getCity());
+                vacToTag.getElementsByClass("companyName").append(vacancy.getCompanyName());
+                vacToTag.getElementsByClass("salary").append(vacancy.getSalary());
+                vacToTag.getElementsByTag("a")
+                        .append(vacancy.getTitle())
+                        .attr("href", vacancy.getUrl());
+                document
+                        .getElementsByClass("template")
+                        .before(vacToTag.outerHtml());
+            }
+
+            return document.html();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Some exception occurred";
+        }
     }
 
     private void updateFile(String content) throws IOException {
@@ -41,5 +75,9 @@ public class HtmlView implements View {
             fos.write(content.getBytes());
             fos.flush();
         }
+    }
+
+    protected Document getDocument() throws IOException {
+        return Jsoup.parse(new File(filePath), "UTF-8");
     }
 }
