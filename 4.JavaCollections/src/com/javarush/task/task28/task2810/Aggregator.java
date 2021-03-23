cec49410b6bafa377850ee53b0ea6b8eda264100
kostya-wolf <kostya-wolf@yandex.ru>
2021-03-23 13:41:34 +0300

    task28.task2810 Aggregator (12, 13, 14)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,6 +1,16 @@
 package com.javarush.task.task28.task2810;
 
+import com.javarush.task.task28.task2810.model.HHStrategy;
+import com.javarush.task.task28.task2810.model.Model;
+import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.view.HtmlView;
+
 public class Aggregator {
     public static void main(String[] args) {
+        HtmlView view = new HtmlView();
+        Provider provider = new Provider(new HHStrategy());
+        Model model = new Model(view, provider);
+        view.setController(new Controller(model));
+        view.userCitySelectEmulationMethod();
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -3,18 +3,43 @@ package com.javarush.task.task28.task2810.view;
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class HtmlView implements View {
     private Controller controller;
+    private final String filePath = "./4.JavaCollections/src/" + this.getClass().getPackage().getName().replace('.', '/') + "/vacancies.html";
 
     @Override
     public void update(List<Vacancy> vacancies) {
-
+        try {
+            String content = getUpdatedFileContent(vacancies);
+            updateFile(content);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void setController(Controller controller) {
         this.controller = controller;
+    }
+
+    public void userCitySelectEmulationMethod() {
+        controller.onCitySelect("Odessa");
+    }
+
+    private String getUpdatedFileContent(List<Vacancy> vacancies) {
+        return null;
+    }
+
+    private void updateFile(String content) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(filePath)) {
+            fos.write(content.getBytes());
+            fos.flush();
+        }
     }
 }
