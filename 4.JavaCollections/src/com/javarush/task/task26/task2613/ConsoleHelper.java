b38d5d1f0605f0b8707b2c69b11cde53432069cf
kostya-wolf <kostya-wolf@yandex.ru>
2021-04-06 19:42:26 +0300

    task26.task2613 CashMachine (2)

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
@@ -1,5 +1,22 @@
 package com.javarush.task.task26.task2613;
 
-public class ConsoleHelper {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class ConsoleHelper {
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String result = null;
+        try {
+            result = bis.readLine();
+        } catch (IOException e) {
+        }
+        return result;
+    }
 }
