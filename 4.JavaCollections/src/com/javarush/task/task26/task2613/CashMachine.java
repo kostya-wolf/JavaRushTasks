6e217495470521f93adcca711744fd6418ab6a73
kostya-wolf <kostya-wolf@yandex.ru>
2021-04-07 16:47:09 +0300

    task26.task2613 CashMachine (4)

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/CashMachine.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CashMachine.java
@@ -1,7 +1,13 @@
 package com.javarush.task.task26.task2613;
 
+import java.util.Locale;
+
 public class CashMachine {
     public static void main(String[] args) {
-
+        Locale.setDefault(Locale.ENGLISH);
+        String curCode = ConsoleHelper.askCurrencyCode();
+        String[] nominalAndCount = ConsoleHelper.getValidTwoDigits(curCode);
+        CurrencyManipulator curMan = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(curCode);
+        curMan.addAmount(Integer.parseInt(nominalAndCount[0]), Integer.parseInt(nominalAndCount[1]));
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
@@ -3,6 +3,7 @@ package com.javarush.task.task26.task2613;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class ConsoleHelper {
     private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
@@ -18,5 +19,40 @@ public class ConsoleHelper {
         } catch (IOException e) {
         }
         return result;
+    }
+
+    public static String askCurrencyCode() {
+        writeMessage("Введите код валюты");
+        String curCode = readString();
+        while (!isCurrencyValid(curCode)) {
+            writeMessage("Данные некорректны. Введите код валюты");
+            curCode = readString();
+        }
+        return curCode.toUpperCase();
+    }
+
+    private static boolean isCurrencyValid(String curCode) {
+        return curCode.length() == 3;
+    }
+
+    public static String[] getValidTwoDigits(String currencyCode) {
+        writeMessage("Введите два целых положительных числа - номинал и количество банкнот");
+        String nominalAndCount = readString();
+        while (!isNominalAndCountValid(nominalAndCount)) {
+            writeMessage("Данные некорректны. Введите два целых положительных числа - номинал и количество банкнот");
+            nominalAndCount = readString();
+        }
+        return nominalAndCount.split(" ");
+    }
+
+    private static boolean isNominalAndCountValid(String nominalAndCount) {
+        try {
+            return Arrays.stream(nominalAndCount.split(" "))
+                    .map(Integer::parseInt)
+                    .filter(d -> d > 0)
+                    .count() == 2;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
