324a795b920f9cf4f0d19e1a25e76e0ebea864e4
kostya-wolf <kostya-wolf@yandex.ru>
2021-04-14 11:01:59 +0300

    task26.task2613 CashMachine (10) SortedMap used

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
@@ -1,7 +1,8 @@
 package com.javarush.task.task26.task2613;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.*;
 
 public class CurrencyManipulator {
     private String currencyCode;
@@ -24,7 +25,11 @@ public class CurrencyManipulator {
     }
 
     public int getTotalAmount() {
-        return denominations.entrySet()
+        return getTotalAmountForMap(denominations);
+    }
+
+    public int getTotalAmountForMap(Map<Integer, Integer> map) {
+        return map.entrySet()
                 .stream()
                 .mapToInt(e -> e.getKey() * e.getValue())
                 .sum();
@@ -32,5 +37,77 @@ public class CurrencyManipulator {
 
     public boolean hasMoney() {
         return getTotalAmount() > 0;
+    }
+
+    public boolean isAmountAvailable(int expectedAmount) {
+        return getTotalAmount() >= expectedAmount;
+    }
+
+    public Map<Integer, Integer> withdrawAmount(int expectedAmount) throws NotEnoughMoneyException {
+        Map<Integer, Integer> bestResultMap = new HashMap<>();
+        bestResultMap = foundBanknotes(expectedAmount, getBanknoteMap(), bestResultMap);
+        if (bestResultMap.isEmpty()) {
+            throw new NotEnoughMoneyException();
+        }
+        bestResultMap.entrySet()
+                .forEach(e -> denominations.put(e.getKey(), denominations.get(e.getKey()) - e.getValue()));
+        return bestResultMap;
+    }
+
+    private SortedMap<Integer, Integer> getBanknoteMap() {
+        SortedMap<Integer, Integer> sortedMap = new TreeMap<>(Collections.reverseOrder());
+        sortedMap.putAll(denominations);
+        return sortedMap;
+    }
+
+    private Map<Integer, Integer> foundBanknotes(int expectedAmount, SortedMap<Integer, Integer> banknoteMap, Map<Integer, Integer> bestResult) {
+        if (banknoteMap != null && !banknoteMap.isEmpty() && expectedAmount <= getTotalAmountForMap(banknoteMap)) {
+            Map<Integer, Integer> result = new HashMap<>();
+            int answerCount = 0;
+            int expectAmTemp = expectedAmount;
+            Map<Integer, Integer> banknoteMapTemp = new TreeMap<>(banknoteMap);
+            for (int nominal : banknoteMapTemp.keySet()) {
+                if (expectAmTemp == 0) break;
+                int countBanknote = expectAmTemp / nominal;
+                if (countBanknote <= banknoteMapTemp.get(nominal)) {
+                    answerCount += countBanknote;
+                    expectAmTemp %= nominal;
+                } else {
+                    countBanknote = banknoteMapTemp.get(nominal);
+                    answerCount += countBanknote;
+                    expectAmTemp -= countBanknote * nominal;
+                }
+                if (countBanknote > 0) {
+                    banknoteMapTemp.put(nominal, banknoteMapTemp.get(nominal) - countBanknote);
+                    result.put(nominal, countBanknote); //возможно здесь добавить к имеющемуся
+                }
+            }
+            if (expectAmTemp == 0 && answerCount > 0) {
+                if (bestResult.isEmpty() || answerCount < getBestAnswer(bestResult)) {
+                    bestResult = result;
+                }
+            }
+            bestResult = foundBanknotes(expectedAmount, getTailMap(banknoteMap), bestResult);
+        }
+        return bestResult;
+    }
+
+    private int getBestAnswer(Map<Integer, Integer> bestResult) {
+        return bestResult.values()
+                .stream()
+                .mapToInt(value -> value)
+                .sum();
+    }
+
+    private SortedMap<Integer, Integer> getTailMap(SortedMap<Integer, Integer> banknoteMap) {
+        if (banknoteMap.size() < 2) {
+            return null;
+        }
+        Iterator<Map.Entry<Integer, Integer>> iterator = banknoteMap.entrySet().iterator();
+        Integer secondKey = banknoteMap.lastKey();
+        for (int i = 0; i < 2 && iterator.hasNext(); i++) {
+            secondKey = iterator.next().getKey();
+        }
+        return banknoteMap.tailMap(secondKey);
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
@@ -1,8 +1,51 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.ConsoleHelper;
+import com.javarush.task.task26.task2613.CurrencyManipulator;
+import com.javarush.task.task26.task2613.CurrencyManipulatorFactory;
+import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.Map;
+
 class WithdrawCommand implements Command {
     @Override
-    public void execute() {
+    public void execute() throws InterruptOperationException {
+        String currencyCode = ConsoleHelper.askCurrencyCode();
+        CurrencyManipulator curMan = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
+        boolean transactionSuccess = true;
+        do {
+            int withdrawSum = askWithdrawSum(curMan);
+            try {
+                Map<Integer, Integer> withdrawMap = curMan.withdrawAmount(withdrawSum);
+                withdrawMap.entrySet()
+                        .stream()
+                        .sorted((e1, e2) -> e2.getKey().compareTo(e1.getKey()))
+                        .forEach(e -> ConsoleHelper.writeMessage(String.format("\t%d - %d", e.getKey(), e.getValue())));
+                ConsoleHelper.writeMessage("Транзакция успешна");
+            } catch (NotEnoughMoneyException e) {
+                ConsoleHelper.writeMessage("Для желаемой суммы не хватает банкнот");
+                transactionSuccess = false;
+            }
+        } while (!transactionSuccess);
+    }
 
+    public static int askWithdrawSum(CurrencyManipulator curMan) throws InterruptOperationException {
+        ConsoleHelper.writeMessage("Введите желаемую сумму");
+        String sum = ConsoleHelper.readString();
+        while (!isWithdrawSumValid(sum, curMan)) {
+            ConsoleHelper.writeMessage("Данные некорректны. Введите желаемую сумму");
+            sum = ConsoleHelper.readString();
+        }
+        return Integer.parseInt(sum);
+    }
+
+    private static boolean isWithdrawSumValid(String sum, CurrencyManipulator curMan) {
+        try {
+            int withdrawSum = Integer.parseInt(sum);
+            return withdrawSum > 0 && curMan.isAmountAvailable(withdrawSum);
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
