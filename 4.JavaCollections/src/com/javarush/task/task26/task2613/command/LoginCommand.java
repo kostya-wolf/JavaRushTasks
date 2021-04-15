d13abfe4111681732919193adf27b48723ff4ad7
kostya-wolf <kostya-wolf@yandex.ru>
2021-04-15 18:11:38 +0300

    task26.task2613 CashMachine (12) ResourceBundle + properties

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/LoginCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/LoginCommand.java
@@ -1,18 +1,23 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.CashMachine;
 import com.javarush.task.task26.task2613.ConsoleHelper;
 import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+
+import java.util.ResourceBundle;
 
 public class LoginCommand implements Command {
     private static final String cardNumber = "123456789012";
     private static final String cardPin = "1234";
+    private ResourceBundle validCreditCards = ResourceBundle.getBundle(CashMachine.class.getPackage().getName() + ".resources.verifiedCards");
 
     @Override
     public void execute() throws InterruptOperationException {
         boolean loginDenied = true;
         do {
             String[] cardNumAndPin = getValidCardNumAndPin();
-            if (cardNumber.equals(cardNumAndPin[0]) && cardPin.equals(cardNumAndPin[1])) {
+            if (validCreditCards.containsKey(cardNumAndPin[0])
+                    && validCreditCards.getString(cardNumAndPin[0]).equals(cardNumAndPin[1])) {
                 ConsoleHelper.writeMessage("Верификация прошла успешно");
                 loginDenied = false;
             }
