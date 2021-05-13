4dce2fc0a43097fb925f91221246a32efc8f814a
kostya-wolf <kostya-wolf@yandex.ru>
2021-05-13 18:59:58 +0300

    task40.task4003 Отправка письма с файлом

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4003/Solution.java
@@ -0,0 +1,73 @@
+package com.javarush.task.task40.task4003;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.Properties;
+
+/* 
+Отправка письма с файлом
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.sendMail("name.lastname@gmail.com", "password", "friend@gmail.com");
+    }
+
+    public void sendMail(final String username, final String password, final String recipients) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients));
+
+            setSubject(message, "Тестовое письмо");
+            setAttachment(message, "c:/text.txt");
+
+            Transport.send(message);
+            System.out.println("Письмо было отправлено.");
+
+        } catch (MessagingException e) {
+            System.out.println("Ошибка при отправке: " + e.toString());
+        }
+    }
+
+    public static void setSubject(Message message, String subject) throws MessagingException {
+        message.setSubject(subject);
+    }
+
+    public static void setAttachment(Message message, String filename) throws MessagingException {
+        // Создание части
+        MimeBodyPart bodyPart = new MimeBodyPart();
+
+        // Добавление файла в часть
+        FileDataSource ds = new FileDataSource(filename);
+        bodyPart.setDataHandler(new DataHandler(ds));
+        bodyPart.setFileName(ds.getName());
+
+        // Создание экземпляра класса Multipart. Добавление части сообщения в него.
+        MimeMultipart multipart = new MimeMultipart();
+        multipart.addBodyPart(bodyPart);
+
+        // Установка экземпляра класса Multipart в качестве контента документа
+        message.setContent(multipart);
+    }
+}
